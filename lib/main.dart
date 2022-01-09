2c2885cb9db33589a3868a7535c342a64ec35c6b
Utsav Shrestha <utsavshrestha@Utsavs-MacBook-Pro.local>
2022-01-10 04:59:10 +0545

    implemented fonts

--- lib/main.dart
+++ lib/main.dart
@@ -97,6 +97,7 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             const Text(
               'You have pushed the button this many times:',
+              style: TextStyle(fontFamily: 'GTWalsheimPro'),
             ),
             Text(
               '$_counter',
