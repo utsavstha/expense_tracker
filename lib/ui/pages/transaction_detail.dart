f1ffb52da151906e679873b79f18d84278be3ffc
Utsav Shrestha <utsavshrestha@Utsavs-MacBook-Pro.local>
2022-01-12 08:16:27 +0545

    finished transaction ui

--- lib/ui/pages/transaction_detail.dart
+++ lib/ui/pages/transaction_detail.dart
@@ -1,3 +1,6 @@
+import 'package:expense_tracker/constants.dart';
+import 'package:expense_tracker/ui/components/dashed_seperator.dart';
+import 'package:expense_tracker/ui/components/primary_button.dart';
 import 'package:expense_tracker/ui/components/ticket_material.dart';
 import 'package:flutter/material.dart';
 
@@ -71,15 +74,16 @@ class _TransactionDetailState extends State<TransactionDetail> {
                             fontWeight: FontWeight.w700),
                       ),
                       const SizedBox(
-                        height: 20,
+                        height: 30,
                       ),
                       Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
                           Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               Text(
-                                'Payee',
+                                'Transaction Type',
                                 style: TextStyle(
                                     fontFamily: 'GTWalsheimPro',
                                     fontSize: 16,
@@ -89,7 +93,7 @@ class _TransactionDetailState extends State<TransactionDetail> {
                                 height: 10,
                               ),
                               Text(
-                                'Breakfast',
+                                'Expense',
                                 style: TextStyle(
                                     color: Colors.black,
                                     fontFamily: 'GTWalsheimPro',
@@ -97,7 +101,89 @@ class _TransactionDetailState extends State<TransactionDetail> {
                                     fontWeight: FontWeight.w700),
                               ),
                             ],
-                          )
+                          ),
+                          Container(
+                              height: 30,
+                              child: VerticalDivider(color: Colors.black)),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Date',
+                                style: TextStyle(
+                                    fontFamily: 'GTWalsheimPro',
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w300),
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                '2020-10-10',
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontFamily: 'GTWalsheimPro',
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w700),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      const DashedSeparator(
+                        color: Colors.grey,
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Amount',
+                                  style: TextStyle(
+                                      fontFamily: 'GTWalsheimPro',
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w300),
+                                ),
+                                Text(
+                                  'Rs 300',
+                                  style: TextStyle(
+                                      fontFamily: 'GTWalsheimPro',
+                                      fontSize: 26,
+                                      fontWeight: FontWeight.w700),
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            height: 40,
+                            child: OutlinedButton(
+                              style: OutlinedButton.styleFrom(
+                                primary: primaryColor,
+                                side:
+                                    BorderSide(width: 1.0, color: primaryColor),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(12.0),
+                                ),
+                              ),
+                              onPressed: () {},
+                              child: const Text(
+                                'Edit',
+                                style: const TextStyle(
+                                    fontFamily: 'GTWalsheimPro',
+                                    fontSize: 14,
+                                    color: primaryColor,
+                                    fontWeight: FontWeight.normal),
+                              ),
+                            ),
+                          ),
                         ],
                       )
                     ],
