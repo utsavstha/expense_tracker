e2a816d2ce764877806c9ecf6c86889e01b8d9e0
Utsav Shrestha <utsavshrestha@Utsavs-MacBook-Pro.local>
2022-01-16 16:58:49 +0545

    finished profile ui

--- lib/ui/pages/profile_page.dart
+++ lib/ui/pages/profile_page.dart
@@ -13,6 +13,7 @@ class _ProfilePageState extends State<ProfilePage> {
     return Scaffold(
       body: SafeArea(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Container(
               decoration: const BoxDecoration(
@@ -33,12 +34,95 @@ class _ProfilePageState extends State<ProfilePage> {
                         fontSize: 20,
                         fontWeight: FontWeight.w700),
                   ),
-                  // const Icon(
-                  //   Icons.calendar_today_rounded,
-                  // )
                 ],
               ),
             ),
+            Card(
+              child: Container(
+                padding: const EdgeInsets.all(16),
+                width: double.infinity,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Container(
+                      width: 100.0,
+                      height: 100.0,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        image: DecorationImage(
+                          image: NetworkImage(
+                              'https://gravatar.com/avatar/1f82b0492a0a938288c2d5b70534a1fb?s=400&d=robohash&r=x'),
+                          fit: BoxFit.cover,
+                        ),
+                        borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                        border: Border.all(
+                          color: Colors.grey,
+                          width: 4.0,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      'Utsav Shrestha',
+                      style: const TextStyle(
+                          fontFamily: 'GTWalsheimPro',
+                          fontSize: 18,
+                          fontWeight: FontWeight.w700),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Email',
+                      style: TextStyle(
+                          fontFamily: 'GTWalsheimPro',
+                          fontSize: 14,
+                          color: Colors.grey,
+                          fontWeight: FontWeight.normal),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Text(
+                      'utsavstha@outlook.com',
+                      style: TextStyle(
+                          fontFamily: 'GTWalsheimPro',
+                          fontSize: 18,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w700),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    const Text(
+                      'Date of birth',
+                      style: TextStyle(
+                          fontFamily: 'GTWalsheimPro',
+                          fontSize: 14,
+                          color: Colors.grey,
+                          fontWeight: FontWeight.normal),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Text(
+                      '1998-10-11',
+                      style: TextStyle(
+                          fontFamily: 'GTWalsheimPro',
+                          fontSize: 18,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w700),
+                    ),
+                  ],
+                ),
+              ),
+            )
           ],
         ),
       ),
