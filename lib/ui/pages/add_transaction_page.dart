2f78c9c870e8e5fb7fde66d26ba0447824a09aa1
Utsav Shrestha <utsavshrestha@Utsavs-MacBook-Pro.local>
2022-01-19 17:33:48 +0545

    finished add tranasction ui

--- lib/ui/pages/add_transaction_page.dart
+++ lib/ui/pages/add_transaction_page.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/ui/components/primary_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -11,6 +12,8 @@ class AddTransactionPage extends StatefulWidget {
 }
 
 class _AddTransactionPageState extends State<AddTransactionPage> {
+  int _currentStep = 0;
+  PageController controller = PageController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,114 +49,269 @@ class _AddTransactionPageState extends State<AddTransactionPage> {
               ],
             ),
           ),
+          LinearProgressIndicator(color: primaryColor, value: 0.5),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
+              child: PageView(
+            controller: controller,
+            children: [StepTwo(), StepOne()],
+          )),
+        ],
+      ),
+    ));
+  }
+}
+
+class StepOne extends StatelessWidget {
+  const StepOne({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 30),
+            Center(
+              child: SvgPicture.asset('assets/saving_illustration.svg',
+                  height: 150),
+            ),
+            const SizedBox(height: 30),
+            const Text(
+              'What kind of \ntransaction is this?',
+              style: TextStyle(
+                  fontFamily: 'GTWalsheimPro',
+                  fontSize: 20,
+                  fontWeight: FontWeight.w700),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            padding: EdgeInsets.all(3.0),
+                            decoration: BoxDecoration(
+                                color: secondaryColor, shape: BoxShape.circle),
+                            child: Icon(
+                              Icons.keyboard_arrow_left_rounded,
+                              color: Colors.white,
+                              size: 40,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 40,
+                          ),
+                          Text(
+                            'Income',
+                            style: TextStyle(
+                                fontFamily: 'GTWalsheimPro',
+                                fontSize: 18,
+                                fontWeight: FontWeight.w700),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            padding: EdgeInsets.all(3.0),
+                            decoration: BoxDecoration(
+                                color: primaryColor, shape: BoxShape.circle),
+                            child: Icon(
+                              Icons.keyboard_arrow_right_rounded,
+                              color: Colors.white,
+                              size: 40,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 40,
+                          ),
+                          Text(
+                            'Expense',
+                            style: TextStyle(
+                                fontFamily: 'GTWalsheimPro',
+                                fontSize: 18,
+                                fontWeight: FontWeight.w700),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class StepTwo extends StatelessWidget {
+  const StepTwo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Container(
+                height: 50,
+                padding: EdgeInsets.all(3.0),
+                decoration: BoxDecoration(
+                    color: secondaryColor, shape: BoxShape.circle),
+                child: Icon(
+                  Icons.keyboard_arrow_left_rounded,
+                  color: Colors.white,
+                  size: 40,
+                ),
+              ),
+              const SizedBox(
+                width: 20,
+              ),
+              Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  const SizedBox(height: 30),
-                  Center(
-                    child: SvgPicture.asset('assets/saving_illustration.svg',
-                        height: 150),
+                  Text(
+                    'Transaction Type',
+                    style: TextStyle(
+                        fontFamily: 'GTWalsheimPro',
+                        fontSize: 16,
+                        fontWeight: FontWeight.w300),
                   ),
-                  const SizedBox(height: 30),
-                  const Text(
-                    'What kind of \ntransaction is this?',
+                  const SizedBox(
+                    height: 6,
+                  ),
+                  Text(
+                    'Expense',
                     style: TextStyle(
                         fontFamily: 'GTWalsheimPro',
                         fontSize: 20,
                         fontWeight: FontWeight.w700),
                   ),
-                  const SizedBox(
-                    height: 40,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Expanded(
-                        child: Card(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(16.0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Container(
-                                  padding: EdgeInsets.all(3.0),
-                                  decoration: BoxDecoration(
-                                      color: secondaryColor,
-                                      shape: BoxShape.circle),
-                                  child: Icon(
-                                    Icons.keyboard_arrow_left_rounded,
-                                    color: Colors.white,
-                                    size: 40,
-                                  ),
-                                ),
-                                const SizedBox(
-                                  height: 40,
-                                ),
-                                Text(
-                                  'Income',
-                                  style: TextStyle(
-                                      fontFamily: 'GTWalsheimPro',
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w700),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      Expanded(
-                        child: Card(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(16.0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Container(
-                                  padding: EdgeInsets.all(3.0),
-                                  decoration: BoxDecoration(
-                                      color: primaryColor,
-                                      shape: BoxShape.circle),
-                                  child: Icon(
-                                    Icons.keyboard_arrow_right_rounded,
-                                    color: Colors.white,
-                                    size: 40,
-                                  ),
-                                ),
-                                const SizedBox(
-                                  height: 40,
-                                ),
-                                Text(
-                                  'Expense',
-                                  style: TextStyle(
-                                      fontFamily: 'GTWalsheimPro',
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w700),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  )
                 ],
-              ),
-            ),
-          )
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 70,
+          ),
+          const Text(
+            'Payee name',
+            style: TextStyle(
+                fontFamily: 'GTWalsheimPro',
+                fontSize: 14,
+                color: Colors.grey,
+                fontWeight: FontWeight.w700),
+          ),
+          const TextField(
+            keyboardType: TextInputType.name,
+            style: TextStyle(
+                fontFamily: 'GTWalsheimPro',
+                fontSize: 14,
+                color: Colors.black,
+                fontWeight: FontWeight.w700),
+            decoration: InputDecoration(
+                border: UnderlineInputBorder(),
+                hintText: 'Enter payee name',
+                hintStyle: TextStyle(
+                    fontFamily: 'GTWalsheimPro',
+                    fontSize: 18,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.w700)),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          const Text(
+            'Amount',
+            style: TextStyle(
+                fontFamily: 'GTWalsheimPro',
+                fontSize: 14,
+                color: Colors.grey,
+                fontWeight: FontWeight.w700),
+          ),
+          const TextField(
+            keyboardType: TextInputType.number,
+            style: TextStyle(
+                fontFamily: 'GTWalsheimPro',
+                fontSize: 14,
+                color: Colors.black,
+                fontWeight: FontWeight.w700),
+            decoration: InputDecoration(
+                border: UnderlineInputBorder(),
+                hintText: '0.00',
+                hintStyle: TextStyle(
+                    fontFamily: 'GTWalsheimPro',
+                    fontSize: 18,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.w700)),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          const Text(
+            'Date',
+            style: TextStyle(
+                fontFamily: 'GTWalsheimPro',
+                fontSize: 14,
+                color: Colors.grey,
+                fontWeight: FontWeight.w700),
+          ),
+          const TextField(
+            keyboardType: TextInputType.number,
+            style: TextStyle(
+                fontFamily: 'GTWalsheimPro',
+                fontSize: 14,
+                color: Colors.black,
+                fontWeight: FontWeight.w700),
+            decoration: InputDecoration(
+                border: UnderlineInputBorder(),
+                hintText: 'Select Date',
+                hintStyle: TextStyle(
+                    fontFamily: 'GTWalsheimPro',
+                    fontSize: 18,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.w700)),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          PrimaryButton(title: "FINISH", color: primaryColor, onPress: null),
         ],
       ),
-    ));
+    );
   }
 }
