465dc5896071a5ae5fd8125e4a6195f1973173d8
Utsav Shrestha <utsavshrestha@Utsavs-MacBook-Pro.local>
2022-01-19 21:56:52 +0545

    added routes

--- lib/main.dart
+++ lib/main.dart
@@ -20,22 +20,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        backgroundColor: Colors.grey.shade100,
-      ),
-      home: AddTransactionPage(),
-    );
+        title: 'Flutter Demo',
+        initialRoute: '/login',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          backgroundColor: Colors.grey.shade100,
+        ),
+        routes: {
+          '/login': (context) => LoginPage(),
+          '/register': (context) => SignUpPage(),
+          '/forgot_password': (context) => ForgotPasswordPage(),
+          '/dashboard': (context) => DashboardPage(),
+          '/transaction_detal': (context) => TransactionDetail(),
+          'add_transaction': (context) => AddTransactionPage()
+        }
+        // home: AddTransactionPage(),
+        );
   }
 }
 
